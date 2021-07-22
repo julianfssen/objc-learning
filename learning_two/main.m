f1b97982ff87a8b8f64c06b56cb65c0444419d3d
julianfoo <julian.foo@investingnote.com>
2021-07-22 20:28:26 +0800

    Initial Commit

---
+++ learning_two/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  learning_two
+//
+//  Created by Foo Siang Sen on 22/07/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
